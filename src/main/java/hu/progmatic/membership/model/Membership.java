17c1aaf1d16dfcd05b9d95d76885919459a5b744
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-13 16:35:10 +0200

    1. create model

---
+++ src/main/java/hu/progmatic/membership/model/Membership.java
@@ -0,0 +1,2 @@
+package hu.progmatic.membership.model;public class Membership {
+}
---
+++ src/test/java/hu/progmatic/membership/MembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmatic.membership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
